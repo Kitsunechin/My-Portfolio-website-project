3c9da237c0c146d75565f7c50d7a91937e9aaf4b
Kitsunechin <martyna.b@hotmail.com>
2020-10-12 22:18:10 +0200

    skills

--- main.js
+++ main.js
@@ -18,13 +18,16 @@ function addProject() {
     let getAll = "";
     for (let index = 0; index<projectArr; index++) {
       getAll+=
-        `<div class= "project"><a class="repo-live" href="${project[index].live}" target="_blank"><img class="project-content" src="${project[index].img}" alt="project screenshot"/></a>
-        <div class ="project-content"><p><span class="style-text">Title: </span>${project[index].project}</p>
+        `<div class= "project">
+        <a class="repo-live" href="${project[index].live}" target="_blank"><img class="project-content" src="${project[index].img}" alt="project screenshot"/></a>
+        <div class ="project-box">
+        <p class="title">${project[index].project}</p>
         <p><span class="style-text">Description: </span>${project[index].description}</p>
-        <p><span class="style-text">Technologies used: </span>${project[index].technologies}
-        <p><a class="repo-live" href="${project[index].live}" target="_blank">Check out the live version here</a>
-        <a class="repo-live repo" href="${project[index].repo}" target="_blank">For the repository go here</a><p>
-        </div></div><hr class="break-clear">`
+        <p><span class="style-text"></span>${project[index].technologies}
+        <div class="button-box">
+        <p><a class="repo-live live" href="${project[index].live}" target="_blank">live version</a>
+        <p><a class="repo-live repo" href="${project[index].repo}" target="_blank">repository</a><p>
+        </div></div></div><hr class="break-clear">`
     }
     $(".projects-box").html(getAll);
 };
--- projects.js
+++ projects.js
@@ -1,59 +1,58 @@
 const project = [
     {
-    img: "images/quiz.png",
-    project: "Quiz App",
-    description: "A simple quiz application developed for my project at Thinkful",
-    technologies:  "mobile-first design and JQuery",
-    live: "https://kitsunechin.github.io/jQuery-Quiz-app",
-    repo: "https://github.com/Kitsunechin/jQuery-Quiz-app"
-    },
-    {
-    img: "images/weather-api.png",
-    project: "Weather Forecast API",
-    description: "Web app developed with API from openweather",
-    technologies:  "ES6, API",
-    live: "https://codepen.io/Foxtato/pen/qvLqKe",
-    repo: "https://github.com/Kitsunechin/Weather-API"   
-    },
-    {
-    img: "images/movie.png",
-    project: "Booking App",
-    description: "Web app for booking movie tickets",
-    technologies:  "ES6, SCSS",
-    live: "https://kitsunechin.github.io/Movie_app",
-    repo: "https://github.com/Kitsunechin/Movie_app"   
-    },
-    {
-    img: "images/portfolio.png",
-    project: "Portfolio website",
-    description: "A simple photography website with routing and photo gallery, contact form and location google API",
-    technologies: "Angular, CSS",
-    live: "https://cdv.martynab.now.sh/",
-    repo: "https://github.com/Kitsunechin/My-Portfolio-website-project"
-    },
-    {
-        img: "images/trail-api.png",
-        project: "Trail Finder",
-        description: "An app which uses geo API, trail API and openweather API for finding trails and weather for a given location",
-        technologies: "j-Query, ES6, API",
-        live: "https://kitsunechin.github.io/Trail_Finder/",
-        repo: "https://github.com/Kitsunechin/Trail_Finder"
+        img: "images/fitter.png",
+        project: "Fitter",
+        description: "An app which allows users to track their exercie routine and awrds medals for keeping your routine",
+        technologies: "React, Node.js, SQL database, ES6, HTML5, CSS3",
+        live: "https://exercise-tracker-capstone-client.vercel.app/",
+        repo: "https://github.com/Kitsunechin/exercise-tracker-capstone-client"
     },
     {
         img: "images/trippster.png",
         project: "Trippster",
         description: "An app which allows users to add countries to their bucket lists along with the notes on the trip",
-        technologies: "React, Node.js, SQL database",
+        technologies: "React, Node.js, SQL database, ES6, HTML5, CSS3",
         live: "https://search-for-country-to-visit-casptone-client.vercel.app/",
         repo: "https://github.com/Kitsunechin/search-for-country-to-visit-casptone-client"
     },
     {
-        img: "images/fitter.png",
-        project: "Fitter",
-        description: "An app which allows users to track their exercie routine and awrds medals for keeping your routine",
-        technologies: "React, Node.js, SQL database",
-        live: "https://exercise-tracker-capstone-client.vercel.app/",
-        repo: "https://github.com/Kitsunechin/exercise-tracker-capstone-client"
+        img: "images/trail-api.png",
+        project: "Trail Finder",
+        description: "An app which uses geo API, trail API and openweather API for finding trails and weather for a given location",
+        technologies: "j-Query, ES6, API, ES6, HTML5, CSS3",
+        live: "https://kitsunechin.github.io/Trail_Finder/",
+        repo: "https://github.com/Kitsunechin/Trail_Finder"
     },
+    {   img: "images/portfolio.png",
+        project: "Portfolio website",
+        description: "A simple photography website with routing and photo gallery, contact form and location google API",
+        technologies: "Angular, CSS, ES6, HTML5, CSS3",
+        live: "https://cdv.martynab.now.sh/",
+        repo: "https://github.com/Kitsunechin/My-Portfolio-website-project"
+    }, 
+    {
+        img: "images/movie.png",
+        project: "Booking App",
+        description: "Web app for booking movie tickets",
+        technologies:  "ES6, SCSS, HTML5",
+        live: "https://kitsunechin.github.io/Movie_app",
+        repo: "https://github.com/Kitsunechin/Movie_app"   
+        },
+        {
+            img: "images/weather-api.png",
+            project: "Weather Forecast API",
+            description: "Web app developed with API from openweather",
+            technologies:  "ES6, API, HTML5, CSS3",
+            live: "https://codepen.io/Foxtato/pen/qvLqKe",
+            repo: "https://github.com/Kitsunechin/Weather-API"   
+        },
+        {
+            img: "images/quiz.png",
+            project: "Quiz App",
+            description: "A simple quiz application developed for my project at Thinkful",
+            technologies:  "ES6, HTML5, CSS3, jQuery",
+            live: "https://kitsunechin.github.io/jQuery-Quiz-app",
+            repo: "https://github.com/Kitsunechin/jQuery-Quiz-app"
+        },
 ];
 
