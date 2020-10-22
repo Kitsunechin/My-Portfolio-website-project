a868025563ad49ed1dc491df7ce9258cd7af4502
Kitsunechin <martyna.b@hotmail.com>
2020-10-22 18:28:15 +0200

    styling projects

--- main.js
+++ main.js
@@ -3,32 +3,37 @@ function displayMenu() {
     $("#js-button-menu").on("click", () => {
         $(".nav-item").toggle("slow");
         $(".button-menu").show();
-        $(window).on('resize', function() {
-            if(!$(".toggle").is(":visible"))
-            {
-                $('.nav-item').show();   
+        $(window).on('resize', function () {
+            if (!$(".toggle").is(":visible")) {
+                $('.nav-item').show();
             }
         });
-    }); 
+    });
 };
 
 // add project from the projects.js file to DOM
 function addProject() {
     const projectArr = project.length;
     let getAll = "";
-    for (let index = 0; index<projectArr; index++) {
-      getAll+=
-        `<div class= "project">
-        <a class="repo-live" href="${project[index].live}" target="_blank"><img class="project-content" src="${project[index].img}" alt="project screenshot"/></a>
+    for (let index = 0; index < projectArr; index++) {
+        getAll += `
+        <div class= "project-image-wrapper">
+            <a class="repo-live" href="${project[index].live}" target="_blank">
+                <img class="project-image" src="${project[index].img}" alt="project screenshot"/>
+            </a>
         </div>
         <div class ="project-content">
-        <p class="title">${project[index].project}</p>
-        <p>${project[index].description}</p>
-        <br>
-        <p class="technologies">${project[index].technologies}</p>
-        <br>
-        <a class="repo-live live" href="${project[index].live}" target="_blank"><i class="fa fa-desktop"></i>demo</a>
-        <a class="repo-live repo" href="${project[index].repo}" target="_blank"><i class="fa fa-code"></i>code</a>
+            <p class="title">${project[index].project}</p>
+            <p>${project[index].description}</p>
+            <br>
+            <p class="technologies">${project[index].technologies}</p>
+            <br>
+            <a class="repo-live live" href="${project[index].live}" target="_blank">
+                <i class="fa fa-desktop"></i>demo
+            </a>
+            <a class="repo-live repo" href="${project[index].repo}" target="_blank">
+                <i class="fa fa-code"></i>code
+            </a>
         </div>
         <hr class="break-clear">
         <br>`
@@ -39,7 +44,7 @@ function addProject() {
 // start the DOM
 function startPortfolio() {
     displayMenu();
-    addProject();    
+    addProject();
 };
 
 $(startPortfolio);
